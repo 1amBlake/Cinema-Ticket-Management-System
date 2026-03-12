5bb2d82986c565a9f773a11d9975608185b65356
Blake <doanminhhuy155k6@gmail.com>
2026-03-12 13:12:30 +0700

    làm file enums MovieStatus và MovieAgeRating

--- cinema-ticket-management-system/src/main/java/com/cinema/enums/MovieAgeRating.java
+++ cinema-ticket-management-system/src/main/java/com/cinema/enums/MovieAgeRating.java
@@ -1,8 +1,64 @@
 package com.cinema.enums;
-/***
- * hihihif
+
+/**
+ * Phân loại độ tuổi của phim.
+ * 0 - P : Mọi lứa tuổi
+ * 1 - K : Trẻ dưới 13 tuổi có thể xem được (có người giám hộ)
+ * 2 - T13: Cấm khán giả dưới 13 tuổi
+ * 3 - T16: Cấm khán giả dưới 16 tuổi
+ * 4 - T18: Cấm khán giả dưới 18 tuổi
+ * 5 - C: Cấm phổ biến
+ * @author minhhuy
  */
 public enum MovieAgeRating {
+    P(0, "P"),
+    K(1, "K"),
+    T13(2, "T13"),
+    T16(3, "T16"),
+    T18(4, "T18"),
+	C(5, "C");
+	
+    private final int ageRatingID;
+    private final String displayMovieAgeRating;
 
-	P
+    /**
+     * Constructor
+     * @param ageRatingId
+     * @param displayName
+     */
+    MovieAgeRating(int ageRatingId, String displayName) {
+        this.ageRatingID = ageRatingId;
+        this.displayMovieAgeRating = displayName;
+    }
+    
+    /**
+     * Lấy mã/id của phần tử, dùng trên hệ thống và lưu trữ dưới database
+     * 
+     * @return int - mã
+     */
+    public int getMovieAgeRatingId() {
+        return ageRatingID;
+    }
+    
+    /**
+     * Phương thức dùng để hiển thị giới hạn tuổi
+     * @return String - Xếp loại tuổi
+     */
+    public String getDisplayMovieAgeRating() {
+        return displayMovieAgeRating;
+    }
+    
+    /**
+     * Phương thúc để đổi id thành giá trị Enum tương ứng
+     * @param arId
+     * @return
+     */
+    public static MovieAgeRating getMovieAgeRatingFromId(int arId) {
+        for (MovieAgeRating rating : values()) {
+            if (rating.ageRatingID == arId) {
+                return rating;
+            }
+        }
+        throw new IllegalArgumentException("Mã giới hạn tuổi không hợp lệ: " + arId);
+    }
 }
--- cinema-ticket-management-system/src/main/java/com/cinema/enums/MovieStatus.java
+++ cinema-ticket-management-system/src/main/java/com/cinema/enums/MovieStatus.java
@@ -1,5 +1,57 @@
 package com.cinema.enums;
 
+/**
+ * Phân loại trạng thái của phim
+ * 0 - COMMINGSOON: Phim sắp được chiếu
+ * 1 - SHOWING: Phim đang được chiếu
+ * 3 - STOPPED: Phim đã bị ngừng chiếu
+ * @author minhhuy
+ */
 public enum MovieStatus {
+	COMMINGSOON(0, "Sắp Chiếu"),
+	SHOWING(1, "Đang Chiếu"),
+	STOPPED(2, "Dừng Chiếu");
+	
+	private final int movieStatusId;
+	private final String displayMovieStatus;
+	
+	/**
+	 * Constructor
+	 * @param movieStatusId
+	 * @param displayMovieStatus
+	 */
+	MovieStatus(int movieStatusId, String displayMovieStatus){
+		this.movieStatusId = movieStatusId;
+		this.displayMovieStatus = displayMovieStatus;
+	}
+	
+	/**
+	 * Lấy mã id của trạng thái phim
+	 * @return int movieStatusId
+	 */
+	public int getMovieStatusId() {
+		return movieStatusId;
+	}
+	
+	/**
+	 * Dùng để hiển thị trạng thái phim.
+	 * Ví dụ: "Sắp Chiếu"
+	 * @return String displayMovieStatus
+	 */
+	public String getDisplayMovieStatus() {
+		return displayMovieStatus;
+	}
 
+	/**
+	 * Dùng để lấy được trạng thái phim thông qua id trạng thái
+	 * @param msId
+	 * @return MovieStatus status
+	 */
+	public static MovieStatus getMovieStatusFromId(int msId) {
+		for (MovieStatus status : values()) {
+			if (status.movieStatusId == msId)
+				return status;
+		}
+		throw new IllegalArgumentException("Mã trạng thái phim không hợp lệ!");
+	}
 }
